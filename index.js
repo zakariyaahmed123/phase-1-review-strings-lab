53190ee5dc432e3f1179c072f333e7749f8d9907
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-13 14:59:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `${currentUser} Welcome to Flatbook, !`;
+const excitedWelcomeMessage = `${currentUser.toUpperCase()} WELCOME TO FLATBOOK, !`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)} !`;
+
